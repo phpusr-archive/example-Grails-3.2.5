9ac582150e2f2a0ba08fccd5be121d1bd4d5c0b8
phpusr <phpusr@gmail.com>
2017-02-08 17:58:04 +0500

    add: quartz

---
+++ grails-app/jobs/grails/test/HelloJob.groovy
@@ -0,0 +1,11 @@
+package grails.test
+
+class HelloJob {
+    static triggers = {
+      simple repeatInterval: 5000l // execute job once in 5 seconds
+    }
+
+    def execute() {
+        log.info '>> Hello user!'
+    }
+}
