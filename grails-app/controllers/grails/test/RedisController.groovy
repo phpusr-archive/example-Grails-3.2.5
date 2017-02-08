3dc829aa1f1bee9b1239e0a2b9990e86e1f0eb1e
phpusr <phpusr@gmail.com>
2017-02-08 18:55:00 +0500

    change: Redis plugin

---
+++ grails-app/controllers/grails/test/RedisController.groovy
@@ -0,0 +1,18 @@
+package grails.test
+
+import redis.clients.jedis.Jedis
+
+class RedisController {
+
+    def redisService
+
+    def index() {
+        redisService.withRedis { Jedis redis ->
+            final keys = redis.keys('tabel:*')
+            log.debug keys.toListString()
+        }
+
+        render 'OK'
+    }
+
+}
